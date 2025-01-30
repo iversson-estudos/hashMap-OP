1ae29431a837bc29074db0843a4c92cc978b9a6c
iversson-estudos <i versson.estudos@gmail.com>
2025-01-30 10:43:56 -0300

    started working on the set function

--- src/classes/HashMap.js
+++ src/classes/HashMap.js
@@ -1,8 +1,37 @@
-class HashMap() {
+class HashMap {
   constructor(loadFactor = 0.75, capacity = 16) {
     this.capacity = capacity;
     this.loadFactor = loadFactor;
     this.buckets = new Array(capacity);
     this.size = 0;
   }
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    if (
+      key == null ||
+      key == undefined ||
+      key == "" ||
+      value == null ||
+      value == undefined ||
+      value == ""
+    ) {
+      return;
+    }
+    const index = this.hash(key);
+
+    if (this.buckets[index][0] === value) {
+    }
+  }
 }
+
+export { HashMap };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+import { HashMap } from "./classes/HashMap";
+
+const newHash = new HashMap();
+
+console.log(newHash.hash("Manolo"));
