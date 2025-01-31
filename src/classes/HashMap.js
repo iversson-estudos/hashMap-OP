47e67ca88ca3de979cf2a03e44ff4517e695a910
iversson-estudos <i versson.estudos@gmail.com>
2025-01-31 17:01:35 -0300

    finished the last functions

--- src/classes/HashMap.js
+++ src/classes/HashMap.js
@@ -48,9 +48,11 @@ class HashMap {
     return 1;
   }
 
-  // Return all buckets.
+  // Return all key value pairs.
   entries() {
-    return this.buckets;
+    return this.buckets.flatMap((bucket) => {
+      return bucket.map(([key, value]) => [key, value]);
+    });
   }
 
   // Clear all key-value pairs.
@@ -67,6 +69,61 @@ class HashMap {
       bucket.forEach(([key, value]) => {
         this.set(key, value);
       });
+    });
+  }
+  /*searches array for key, returns true/false if found */
+  has(key) {
+    if (!key) return 0;
+
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    const found = bucket.find((element) => element[0] === key);
+
+    if (found) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  /*removes if key found*/
+  remove(key) {
+    if (!key) return 0;
+
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    const found = bucket.find((element) => element[0] === key);
+
+    if (found) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          bucket[i] = [];
+          this.storedkeys -= 1;
+          return 1;
+        }
+      }
+    } else {
+      return 0;
+    }
+  }
+  /*returns number of stored keys */
+  length() {
+    return this.storedkeys;
+  }
+
+  getbuckets() {
+    return this.buckets;
+  }
+  /*returns all keys */
+  keys() {
+    return this.buckets.flatMap((bucket) => {
+      return bucket.map(([key, value]) => key);
+    });
+  }
+
+  /*returns all values */
+  values() {
+    return this.buckets.flatMap((bucket) => {
+      return bucket.map(([key, value]) => value);
     });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import "./styles.css";
 import { HashMap } from "./classes/HashMap";
-
-const newHash = new HashMap();
-
-newHash.set("MIqueias", 29);
-newHash.set("Iversson", 30);
-newHash.set("M233", 29);
-newHash.set("IlAND", 30);
-
-console.log(newHash.entries());
